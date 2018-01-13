65f3212203af9ecda602db8f8b2fe579cb33e606
Alexander Kozlenko <alexander.kozlenko@live.com>
2018-01-13 15:34:38 +0200

    Improve source code of the unit tests project

--- src/Community.RandomOrg.Tests/Internal/RandomOrgConvert.cs
+++ src/Community.RandomOrg.Tests/Internal/RandomOrgConvert.cs
@@ -1,16 +1,26 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Globalization;
 using Community.RandomOrg.Data;
 
 namespace Community.RandomOrg.Tests.Internal
 {
+    /// <summary>Represents a converter for RANDOM.ORG JSON values.</summary>
+    [DebuggerStepThrough]
     internal static class RandomOrgConvert
     {
+        /// <summary>Converts a string value to a <see cref="DateTime" /> value.</summary>
+        /// <param name="value">A string with a serialized date and time value.</param>
+        /// <returns>A date and time value.</returns>
         public static DateTime ToDateTime(string value)
         {
             return DateTime.ParseExact(value, "yyyy'-'MM'-'dd' 'HH':'mm':'ss.FFFFFFFK", CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal);
         }
 
+        /// <summary>Converts a string value to a <see cref="ApiKeyStatus" /> value.</summary>
+        /// <param name="value">A string with a serialized API key status value.</param>
+        /// <returns>A <see cref="ApiKeyStatus" /> value.</returns>
+        /// <exception cref="ArgumentException"><paramref name="value" /> contains invalid value for API key status.</exception>
         public static ApiKeyStatus ToApiKeyStatus(string value)
         {
             switch (value)
@@ -29,7 +39,7 @@ namespace Community.RandomOrg.Tests.Internal
                     }
                 default:
                     {
-                        throw new NotSupportedException($"The specified value is not supported: \"{value}\"");
+                        throw new ArgumentException($"The specified value is not supported: \"{value}\"", nameof(value));
                     }
             }
         }
--- src/Community.RandomOrg.Tests/Internal/RandomOrgHandler.cs
+++ src/Community.RandomOrg.Tests/Internal/RandomOrgHandler.cs
@@ -5,10 +5,13 @@ using System.Threading.Tasks;
 
 namespace Community.RandomOrg.Tests.Internal
 {
+    /// <summary>An HTTP mesage handler for <see cref="RandomOrgClient" /> testing.</summary>
     internal sealed class RandomOrgHandler : HttpMessageHandler
     {
         private readonly Func<HttpRequestMessage, Task<HttpResponseMessage>> _handler;
 
+        /// <summary>Initializes a new instance of the <see cref="RandomOrgHandler" /> class.</summary>
+        /// <param name="handler">The handler function.</param>
         public RandomOrgHandler(Func<HttpRequestMessage, Task<HttpResponseMessage>> handler = null)
         {
             _handler = handler;
