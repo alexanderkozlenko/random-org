bb76bcc7217bf5abd508138997ce02701144bc0e
Alexander Kozlenko <alexanderkozlenko@users.noreply.github.com>
2019-09-28 19:02:17 +0300

    Remove TypeInfo (#36)

--- src/Anemonis.RandomOrg/Resources/Strings.cs
+++ src/Anemonis.RandomOrg/Resources/Strings.cs
@@ -9,7 +9,7 @@ namespace Anemonis.RandomOrg.Resources
     internal static class Strings
     {
         private static readonly ResourceManager _resourceManager =
-            new ResourceManager(typeof(Strings).Namespace + "." + nameof(Strings), typeof(Strings).GetTypeInfo().Assembly);
+            new ResourceManager(typeof(Strings).Namespace + "." + nameof(Strings), typeof(Strings).Assembly);
 
         public static string GetString(string name)
         {
