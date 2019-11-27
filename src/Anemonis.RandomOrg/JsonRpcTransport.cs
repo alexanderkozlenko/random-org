889592c27a275f3e94b02f6386c30c98f6aa8f07
Alexander Kozlenko <alexander.kozlenko@live.com>
2019-11-27 04:04:23 +0200

    Update JSON-RPC transport settings

---
+++ src/Anemonis.RandomOrg/JsonRpcTransport.cs
@@ -0,0 +1,13 @@
+﻿// © Alexander Kozlenko. Licensed under the MIT License.
+
+using System.Text;
+
+namespace Anemonis.RandomOrg
+{
+    internal static class JsonRpcTransport
+    {
+        public const string MediaType = "application/json";
+
+        public static readonly string Charset = Encoding.UTF8.WebName;
+    }
+}
