2b4b28f1aa9a63e7abe75d5a8fca98e52fe0fa9a
Alexander Kozlenko <alexanderkozlenko@users.noreply.github.com>
2019-11-30 11:44:11 +0200

    Add User-Agent hedaer (#55)

--- src/Anemonis.RandomOrg/RandomOrgClient.cs
+++ src/Anemonis.RandomOrg/RandomOrgClient.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -24,12 +24,8 @@ namespace Anemonis.RandomOrg
     /// <summary>Represents RANDOM.ORG service client.</summary>
     public sealed partial class RandomOrgClient : IDisposable
     {
-        private static readonly MediaTypeHeaderValue _contentTypeHeaderValue =
-            MediaTypeHeaderValue.Parse($"{JsonRpcTransport.MediaType}; charset={JsonRpcTransport.Charset}");
-        private static readonly MediaTypeWithQualityHeaderValue _acceptHeaderValue =
-            MediaTypeWithQualityHeaderValue.Parse(JsonRpcTransport.MediaType);
-        private static readonly StringWithQualityHeaderValue _acceptCharsetHeaderValue =
-            StringWithQualityHeaderValue.Parse(JsonRpcTransport.Charset);
+        private static readonly string _contentTypeHeaderValue = $"{JsonRpcTransport.MediaType}; charset={JsonRpcTransport.Charset}";
+        private static readonly string _userAgentHeaderValue = CreateUserAgentHeaderValue();
 
         private static readonly Uri _serviceUri = new Uri("https://api.random.org/json-rpc/3/invoke", UriKind.Absolute);
         private static readonly JsonSerializer _jsonSerializer = CreateJsonSerializer();
@@ -93,6 +89,13 @@ namespace Anemonis.RandomOrg
             return JsonSerializer.Create(settings);
         }
 
+        private static string CreateUserAgentHeaderValue()
+        {
+            var assemblyName = Assembly.GetExecutingAssembly().GetName();
+
+            return $"{nameof(Anemonis)}/{assemblyName.Version.ToString(2)} (nuget:{assemblyName.Name})";
+        }
+
         private static HttpMessageInvoker CreateHttpInvoker()
         {
             var httpHandler = new HttpClientHandler
@@ -101,14 +104,7 @@ namespace Anemonis.RandomOrg
                 AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
             };
 
-            var httpClient = new HttpClient(httpHandler);
-
-            httpClient.DefaultRequestHeaders.Accept.Add(_acceptHeaderValue);
-            httpClient.DefaultRequestHeaders.AcceptCharset.Add(_acceptCharsetHeaderValue);
-            httpClient.DefaultRequestHeaders.ExpectContinue = false;
-            httpClient.Timeout = TimeSpan.FromMinutes(2);
-
-            return httpClient;
+            return new HttpClient(httpHandler);
         }
 
         private static JsonRpcContractResolver CreateJsonRpcContractResolver()
@@ -274,9 +270,14 @@ namespace Anemonis.RandomOrg
                 {
                     var requestContent = new StreamContent(requestStream);
 
-                    requestContent.Headers.ContentType = _contentTypeHeaderValue;
+                    requestContent.Headers.Add("Content-Type", _contentTypeHeaderValue);
+
                     httpRequest.Content = requestContent;
                     httpRequest.Headers.Date = DateTime.UtcNow;
+                    httpRequest.Headers.ExpectContinue = false;
+                    httpRequest.Headers.Add("Accept", JsonRpcTransport.MediaType);
+                    httpRequest.Headers.Add("Accept-Charset", JsonRpcTransport.Charset);
+                    httpRequest.Headers.Add("User-Agent", _userAgentHeaderValue);
 
                     using (var httpResponse = await _httpInvoker.SendAsync(httpRequest, cancellationToken).ConfigureAwait(false))
                     {
@@ -291,7 +292,7 @@ namespace Anemonis.RandomOrg
                         {
                             throw new RandomOrgProtocolException(httpResponse.StatusCode, Strings.GetString("protocol.http.headers.content_type.invalid_value"));
                         }
-                        if (!contentTypeHeaderValue.MediaType.Equals(_contentTypeHeaderValue.MediaType, StringComparison.OrdinalIgnoreCase))
+                        if (!contentTypeHeaderValue.MediaType.Equals(JsonRpcTransport.MediaType, StringComparison.OrdinalIgnoreCase))
                         {
                             throw new RandomOrgProtocolException(httpResponse.StatusCode, Strings.GetString("protocol.http.headers.content_type.invalid_value"));
                         }
