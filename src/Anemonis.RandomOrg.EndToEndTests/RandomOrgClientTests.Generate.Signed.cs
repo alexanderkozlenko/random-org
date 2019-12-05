0618136f39f96329366ee3e860f96272083ecad5
Alexander Kozlenko <alexanderkozlenko@users.noreply.github.com>
2019-12-05 10:40:03 +0200

    Add end-to-end tests (#60)
    * Add end-to-end tests
    
    * Fix 'dotnet test' step argument

---
+++ src/Anemonis.RandomOrg.EndToEndTests/RandomOrgClientTests.Generate.Signed.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Anemonis.RandomOrg.EndToEndTests
+{
+    public partial class RandomOrgClientTests
+    {
+        [TestMethod]
+        public async Task GenerateSignedBlobsAsync()
+        {
+            using var client = new RandomOrgClient(GetServiceApiKey());
+
+            var result = await client.GenerateSignedBlobsAsync(2, 2).ConfigureAwait(false);
+
+            AssertSignedRandomResult(result);
+
+            Assert.AreEqual(2, result.Random.Data.Count);
+            Assert.IsNotNull(result.Random.Data[0]);
+            Assert.IsNotNull(result.Random.Data[1]);
+            Assert.AreEqual(2, result.Random.Data[0].Length);
+            Assert.AreEqual(2, result.Random.Data[1].Length);
+
+            TraceSignedRandomResult(result);
+
+            Trace.WriteLine($"result.random.data.0: {BitConverter.ToString(result.Random.Data[0])}");
+            Trace.WriteLine($"result.random.data.1: {BitConverter.ToString(result.Random.Data[1])}");
+        }
+
+        [TestMethod]
+        public async Task GenerateSignedDecimalFractionsAsync()
+        {
+            using var client = new RandomOrgClient(GetServiceApiKey());
+
+            var result = await client.GenerateSignedDecimalFractionsAsync(2, 2, false).ConfigureAwait(false);
+
+            AssertSignedRandomResult(result);
+
+            Assert.AreEqual(2, result.Random.Data.Count);
+
+            TraceSignedRandomResult(result);
+
+            Trace.WriteLine($"result.random.data.0: {result.Random.Data[0]:N2}");
+            Trace.WriteLine($"result.random.data.1: {result.Random.Data[1]:N2}");
+        }
+
+        [TestMethod]
+        public async Task GenerateSignedGaussiansAsync()
+        {
+            using var client = new RandomOrgClient(GetServiceApiKey());
+
+            var result = await client.GenerateSignedGaussiansAsync(2, 0.0m, 1.0m, 2).ConfigureAwait(false);
+
+            AssertSignedRandomResult(result);
+
+            Assert.AreEqual(2, result.Random.Data.Count);
+
+            TraceSignedRandomResult(result);
+
+            Trace.WriteLine($"result.random.data.0: {result.Random.Data[0]:N2}");
+            Trace.WriteLine($"result.random.data.1: {result.Random.Data[1]:N2}");
+        }
+
+        [TestMethod]
+        public async Task GenerateSignedIntegersAsync()
+        {
+            using var client = new RandomOrgClient(GetServiceApiKey());
+
+            var result = await client.GenerateSignedIntegersAsync(2, 0, 9, false).ConfigureAwait(false);
+
+            AssertSignedRandomResult(result);
+
+            Assert.AreEqual(2, result.Random.Data.Count);
+
+            TraceSignedRandomResult(result);
+
+            Trace.WriteLine($"result.random.data.0: {result.Random.Data[0]}");
+            Trace.WriteLine($"result.random.data.1: {result.Random.Data[1]}");
+        }
+
+        [TestMethod]
+        public async Task GenerateSignedIntegerSequencesAsync()
+        {
+            using var client = new RandomOrgClient(GetServiceApiKey());
+
+            var result = await client.GenerateSignedIntegerSequencesAsync(new[] { 2, 2 }, new[] { 0, 0 }, new[] { 9, 9 }, new[] { false, false }).ConfigureAwait(false);
+
+            AssertSignedRandomResult(result);
+
+            Assert.AreEqual(2, result.Random.Data.Count);
+
+            TraceSignedRandomResult(result);
+
+            Trace.WriteLine($"result.random.data.0.0: {result.Random.Data[0][0]}");
+            Trace.WriteLine($"result.random.data.0.1: {result.Random.Data[0][1]}");
+            Trace.WriteLine($"result.random.data.1.0: {result.Random.Data[1][0]}");
+            Trace.WriteLine($"result.random.data.1.1: {result.Random.Data[1][1]}");
+        }
+
+        [TestMethod]
+        public async Task GenerateSignedStringsAsync()
+        {
+            using var client = new RandomOrgClient(GetServiceApiKey());
+
+            var result = await client.GenerateSignedStringsAsync(2, 2, "abcdef", false).ConfigureAwait(false);
+
+            AssertSignedRandomResult(result);
+
+            Assert.AreEqual(2, result.Random.Data.Count);
+            Assert.IsNotNull(result.Random.Data[0]);
+            Assert.IsNotNull(result.Random.Data[1]);
+
+            TraceSignedRandomResult(result);
+
+            Trace.WriteLine($"result.random.data.0: {result.Random.Data[0]}");
+            Trace.WriteLine($"result.random.data.1: {result.Random.Data[1]}");
+        }
+
+        [TestMethod]
+        public async Task GenerateSignedUuidsAsync()
+        {
+            using var client = new RandomOrgClient(GetServiceApiKey());
+
+            var result = await client.GenerateSignedUuidsAsync(2).ConfigureAwait(false);
+
+            AssertSignedRandomResult(result);
+
+            Assert.AreEqual(2, result.Random.Data.Count);
+
+            TraceSignedRandomResult(result);
+
+            Trace.WriteLine($"result.random.data.0: {result.Random.Data[0]}");
+            Trace.WriteLine($"result.random.data.1: {result.Random.Data[1]}");
+        }
+    }
+}
---
+++ src/Anemonis.RandomOrg.EndToEndTests/RandomOrgClientTests.Internal.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using Anemonis.RandomOrg.Data;
+using Anemonis.Runtime.Configuration;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Anemonis.RandomOrg.EndToEndTests
+{
+    public partial class RandomOrgClientTests
+    {
+        private static readonly AssemblyConfiguration _configuration = AssemblyConfiguration.Load(Assembly.GetExecutingAssembly(), "svcconfig");
+
+        private static string GetServiceApiKey()
+        {
+            var serviceApiKey = _configuration.GetString("random:apiKey");
+
+            if (serviceApiKey == string.Empty)
+            {
+                Assert.Inconclusive();
+            }
+
+            return serviceApiKey;
+        }
+
+        private static void AssertRandomResult<TValue>(RandomResult<TValue> result)
+        {
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Random);
+            Assert.IsNotNull(result.Random.Data);
+            Assert.IsTrue(result.BitsUsed >= 0L);
+            Assert.IsTrue(result.BitsLeft >= 0L);
+            Assert.IsTrue(result.RequestsLeft >= 0L);
+            Assert.IsTrue(result.AdvisoryDelay.Ticks >= 0L);
+        }
+
+        private static void AssertSignedRandomResult<TValue, TParameters>(SignedRandomResult<TValue, TParameters> result)
+            where TParameters : RandomParameters, new()
+        {
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Random);
+            Assert.IsNotNull(result.Random.Data);
+            Assert.IsTrue(result.BitsUsed >= 0L);
+            Assert.IsTrue(result.BitsLeft >= 0L);
+            Assert.IsTrue(result.RequestsLeft >= 0L);
+            Assert.IsTrue(result.AdvisoryDelay.Ticks >= 0L);
+
+            var signature = result.GetSignature();
+
+            Assert.IsNotNull(signature);
+            Assert.AreEqual(512, signature.Length);
+        }
+
+        private static void TraceRandomResult<T>(RandomResult<T> result)
+        {
+            Trace.WriteLine($"result.bits_used: {result.BitsUsed}");
+            Trace.WriteLine($"result.bits_left: {result.BitsLeft}");
+            Trace.WriteLine($"result.requests_left: {result.RequestsLeft}");
+            Trace.WriteLine($"result.advisory_delay: {result.AdvisoryDelay}");
+            Trace.WriteLine($"result.random.completion_time: {result.Random.CompletionTime:yyyy.MM.dd-HH:mm:ss.ffff}");
+        }
+
+        private static void TraceSignedRandomResult<TValue, TParameters>(SignedRandomResult<TValue, TParameters> result)
+            where TParameters : RandomParameters, new()
+        {
+            var signature = result.GetSignature();
+
+            Trace.WriteLine($"result.signature: {BitConverter.ToString(signature).Replace("-", string.Empty)}");
+            Trace.WriteLine($"result.bits_used: {result.BitsUsed}");
+            Trace.WriteLine($"result.bits_left: {result.BitsLeft}");
+            Trace.WriteLine($"result.requests_left: {result.RequestsLeft}");
+            Trace.WriteLine($"result.advisory_delay: {result.AdvisoryDelay}");
+            Trace.WriteLine($"result.random.completion_time: {result.Random.CompletionTime:yyyy.MM.dd-HH:mm:ss.ffff}");
+        }
+    }
+}
